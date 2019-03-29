e1658711d23537bf19d595bc07a7bca30f08e6ed
Katherine Saldivar <katherinekaufmann1@gmail.com>
2019-03-28 21:30:30 -0700

    worked on navbar

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $('#navbar').html('<ul>'+
+    '<li>Healthy Rise Bakery</li>'+
+    '<li><a href = "#home"> Home</a></li>'+
+    '<li><a href="#about">About</a></li>'+
+    '<li><a href="./menu.html">Menu</a></li>'+
+    '<li><a href="#bread-bus">Bread Bus</a></li>'+
+    '<li><a href="#coffee-shop">Shops</a></li>'+
+    '<li><a href="#gallery">Gallery</a></li>'+
+    '<li><a href="#contact">Contact</a></li>'+
+    '</ul>');
+
+});
