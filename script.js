39efd4001af161a08976991bc66291edfa7b2e24
Katherine Saldivar <katherinekaufmann1@gmail.com>
2019-03-29 21:48:54 -0700

    css tweaks

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 
     $('#navbar').html('<ul>'+
-    '<li>Healthy Rise Bakery</li>'+
-    '<li><a href = "#home"> Home</a></li>'+
-    '<li><a href="#about">About</a></li>'+
+    '<li><a href = "./index.html#home"> <img id="nav-logo" src="/images/logo-crop.webp" alt="Healthy Rise logo"> </a></li>'+
+    '<li><a href = "./index.html#home"> Home</a></li>'+
+    '<li><a href="./index.html#about">About</a></li>'+
     '<li><a href="./menu.html">Menu</a></li>'+
-    '<li><a href="#bread-bus">Bread Bus</a></li>'+
-    '<li><a href="#coffee-shop">Shops</a></li>'+
-    '<li><a href="#gallery">Gallery</a></li>'+
-    '<li><a href="#contact">Contact</a></li>'+
+    '<li><a href="./index.html#bread-bus">Bread Bus</a></li>'+
+    '<li><a href="./index.html#coffee-shop">Shops</a></li>'+
+    '<li><a href="./index.html#gallery">Gallery</a></li>'+
+    '<li><a href="./index.html#contact">Contact</a></li>'+
     '</ul>');
 
 });
