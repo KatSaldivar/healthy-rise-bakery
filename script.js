087ee313d38c50c7dbd4149eb71a5813532298a1
Katherine Saldivar <katherinekaufmann1@gmail.com>
2019-03-29 21:55:05 -0700

    nav tweaks

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 
     $('#navbar').html('<ul>'+
     '<li><a href = "./index.html#home"> <img id="nav-logo" src="/images/logo-crop.webp" alt="Healthy Rise logo"> </a></li>'+
-    '<li><a href = "./index.html#home"> Home</a></li>'+
-    '<li><a href="./index.html#about">About</a></li>'+
-    '<li><a href="./menu.html">Menu</a></li>'+
-    '<li><a href="./index.html#bread-bus">Bread Bus</a></li>'+
-    '<li><a href="./index.html#coffee-shop">Shops</a></li>'+
-    '<li><a href="./index.html#gallery">Gallery</a></li>'+
-    '<li><a href="./index.html#contact">Contact</a></li>'+
+    '<li class = "nav-text"><a href = "./index.html#home"> Home</a></li>'+
+    '<li class = "nav-text"><a href="./index.html#about">About</a></li>'+
+    '<li class = "nav-text"><a href="./menu.html">Menu</a></li>'+
+    '<li class = "nav-text"><a href="./index.html#bread-bus">Bread Bus</a></li>'+
+    '<li class = "nav-text"><a href="./index.html#coffee-shop">Shops</a></li>'+
+    '<li class = "nav-text"><a href="./index.html#gallery">Gallery</a></li>'+
+    '<li class = "nav-text"><a href="./index.html#contact">Contact</a></li>'+
     '</ul>');
 
 });
